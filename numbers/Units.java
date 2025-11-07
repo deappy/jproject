5389bf39a459f4db759404876fe165ba83e85a6d
Akshay Raju <166707593+deappy@users.noreply.github.com>
2025-11-07 23:49:57 +0530

    complete

--- numbers/Units.java
+++ numbers/Units.java
@@ -2,17 +2,28 @@ import java.util.Scanner;
 
 public class Units {
     public static void getUnitsPlace(double n){
-        System.out.println("The units place is: "+(n%10));
+        System.out.println("The units place is: "+((int)n%10));
     }
     public static void getTensPlace(double n ){
-        System.out.println("The tens place is: "+((n/10)%10));
+        System.out.println("The tens place is: "+(((int)n/10)%10));
     }
     public static void getHundredsPlace(double n){
-        System.out.println("The hundreds place is: "+((n/100)%10));
+        System.out.println("The hundreds place is: "+(((int)n/100)%10));
     }
     public static void getThousandsPlace(double n){
-        System.out.println("The thosands place is: "+((n/1000)%10));
+        System.out.println("The thosands place is: "+(((int)n/1000)%10));
     }
+    public static void getTenthsPlace(double n){
+        System.out.println("The tenths place: "+((int)(Math.abs(n)*10))%10);
+    }
+    public static void getHundredthsPlace(double n){
+        System.out.println("The hundredth place: "+((int)(Math.abs(n)*100))%10);
+    }
+    public static void getThousandthsPlace(double n){
+        System.out.println('The thousandths place: '+((int)(Math.abs(n)*1000))%10);
+    }
+
+
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the 4 digit number with 3 digit decimal: ");
@@ -22,6 +33,10 @@ public class Units {
         getTensPlace(n);
         getHundredsPlace(n);
         getThousandsPlace(n);
+        
+        getTenthsPlace(n);
+        getHundredthsPlace(n);
+        getThousandthsPlace()
         sc.close();
     }
 }

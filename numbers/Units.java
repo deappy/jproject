b759bed07b05b4cb1ac46fade1f361976df2a174
Akshay Raju <166707593+deappy@users.noreply.github.com>
2025-11-07 13:25:52 +0530

    double

--- numbers/Units.java
+++ numbers/Units.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 
 public class Units {
-    public static void getUnitsPlace(int n){
+    public static void getUnitsPlace(double n){
         System.out.println("The units place is: "+(n%10));
     }
-    public static void getTensPlace(int n ){
+    public static void getTensPlace(double n ){
         System.out.println("The tens place is: "+((n/10)%10));
     }
-    public static void getHundredsPlace(int n){
+    public static void getHundredsPlace(double n){
         System.out.println("The hundreds place is: "+((n/100)%10));
     }
-    public static void getThousandsPlace(int n){
+    public static void getThousandsPlace(double n){
         System.out.println("The thosands place is: "+((n/1000)%10));
     }
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the 4 digit number with 3 digit: ");
-        int n =sc.nextInt();
+        System.out.println("Enter the 4 digit number with 3 digit decimal: ");
+        double n =sc.nextDouble();
         
         getUnitsPlace(n);
         getTensPlace(n);
